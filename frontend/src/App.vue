2df46bd292c2ecc4588cf021b83dd037b208d336
jinho.myung <jinho.myung@intusurg.com>
2022-03-15 16:13:01 -0700

    install bootstrap-vue-3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,11 @@
 <template>
-  <div class="container">
-    Hello Todos
-  </div>
+
 </template>
 
 <script>
-
 export default {
   name: 'App',
   components: {
   }
 }
 </script>
-
-<style>
-</style>
