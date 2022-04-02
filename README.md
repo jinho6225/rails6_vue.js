95826a90aa2238f6a587c5a5f5a04299e8204e41
jinho.myung <jinho.myung@intusurg.com>
2022-04-01 22:29:22 -0700

    add Dockerfile for vuejs(frontend) update docker-compose.yml update entrypoint.sh file in order to db:migrate

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Ruby on Rails 6 + Vue.js
+
+docker-compose -f docker-compose.yml up --build
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,4 +6,8 @@ if [ -f tmp/pids/server.pid ]; then
     rm tmp/pids/server.pid
 fi
 
-exec "$@"
+echo "Run migrations"
+bundle exec rails db:migrate
+
+echo "Starting app server ...3000"
+bundle exec rails s -p 3000 -b '0.0.0.0'
